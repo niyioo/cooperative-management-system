7c5f679a394df6aeb673fd5b0548cf931b228235
Niyi Olagbegi <niyiolagbegi@gmail.com>
2026-03-20 05:52:22 +0100

    fix: added missing name attribute to MembersConfig

--- backend/apps/members/apps.py
+++ backend/apps/members/apps.py
@@ -2,7 +2,9 @@ from django.apps import AppConfig
 
 class MembersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    verbose_name = 'apps.members'
+    # ✅ THIS IS THE MISSING LINE
+    name = 'apps.members'
+    verbose_name = 'Members Directory'
 
     def ready(self):
         import apps.members.signals
